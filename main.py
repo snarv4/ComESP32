ad37702922ae1fc901d037825fea631adc857c06
snarv4 <snarvaez@gbmpaulovi.edu.ec>
2026-02-25 16:57:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,38 @@ PUERTO = 80
 
 #--- CLASE DE DATOS (Para Practicar)
 class DatosSensor:
-    def __init__(self, valor)
+    def __init__(self, valor):
         self.valor = valor
 
 # --- Logica de RED
 sock = socket(socket.AF_INET. socket.SOCK_STREAM)
+
+def conectar():
+    try:
+        sock.connect((IP_ESP32, PUERTO))
+        print("Conectado al ESP32")
+        threading.Thread(target=recibir_datos, daemon=True).start()
+    except:
+        print("Error al conectar")
+def recibir_datos():
+    while True:
+        try:
+            data = sock.recv(1024).decode().strip()
+            if data:
+                #Instanciamos la clase para procesar el valor
+                sensor = DatosSensor(int(data))
+                #Actualizar la barra en la interfaz
+                progress['value'] = sensor.valor
+                lbl_valor.config(text=f"Potenciometro: {sensor.valor}")
+        except:
+            break
+
+def led_on(): sock.send(b'ON')
+def led_off(): sock.send(b'OFF')
+
+# --- INTERFAZ GRAFICA ---
+root = tk.Tk()
+root.title("Control ESP32")
+
+#Diseño con GRID
+tk.Label(root)
